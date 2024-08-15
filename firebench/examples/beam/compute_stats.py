e3655a79f3e3de1ffa3f71d1ef9ac853f2182350
The firebench Authors <no-reply@google.com>
2024-08-15 10:13:24 -0700

    Code update
    PiperOrigin-RevId: 663350881

--- firebench/examples/beam/compute_stats.py
+++ firebench/examples/beam/compute_stats.py
@@ -1,3 +1,17 @@
+# Copyright 2024 The firebench Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 r"""Calculates min/max/mean values for a Swirl-LM dataset.
 
 For an overview of running Apache Beam pipelines on Google Cloud, see:
@@ -63,24 +77,6 @@ Steps to run:
 
 """
 
-# BEGIN GOOGLE-INTERNAL
-#
-# Build:
-#
-#   blaze build //third_party/py/firebench/examples/beam:compute_stats.par
-#
-# Run:
-#
-# ./blaze-bin/third_party/py/firebench/examples/beam/compute_stats.par \
-#   --input_path=/bigstore/firebench/<path_to_zarr> \
-#   --output_path=/bigstore/<path_to_output_zarr> \
-#   --pipeline_options=--runner=google3.pipeline.flume.py.runner.FlumeRunner \
-#   --flume_use_batch_scheduler \
-#   --flume_borg_accounting_charged_user_name=sim-research \
-#   --flume_borg_cells=<cell>
-#
-# Took about 10 minutes to run on u6/ramp7.5/fire.zarr (1.1M files, 2.9TiB).
-# END GOOGLE-INTERNAL
 
 from typing import Tuple
 
